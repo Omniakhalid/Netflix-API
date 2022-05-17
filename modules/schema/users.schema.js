77717a096b981e5e24a70c94d110017c5d6f4c87
Omniakhalid <63504895+Omniakhalid@users.noreply.github.com>
2022-05-17 20:59:25 +0200

    Update users.schema.js

--- modules/schema/users.schema.js
+++ modules/schema/users.schema.js
@@ -5,6 +5,7 @@ const usersSchema = new mongoose.Schema(
     userName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
+    image: { type: String },
     phone: { type: Number },
     role: { type: String, default: "user" },
   },
